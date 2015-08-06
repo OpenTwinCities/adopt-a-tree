f98d0d7559310e954eeb9acc581ef847a6c81e14
Eric Ebbesen <Eric.Ebbesen@govdelivery.com>
2015-08-06 18:48:07 -0500

    Adding mailed_at columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208161131) do
+ActiveRecord::Schema.define(version: 20150806234209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 20150208161131) do
     t.string   "awareness_code"
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "emailed_at"
+    t.datetime "mailed_token_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
