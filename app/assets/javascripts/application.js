4170e36158e23eebf5a85d14cd1521525bed157a
Bill Bushey <wbushey@gmail.com>
2016-07-16 20:06:11 -0500

    Using jquery-rails version of jQuery Using jquery-ui-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery
+//= require jquery-ui
 //= require react
 //= require react_ujs
 //= require components
