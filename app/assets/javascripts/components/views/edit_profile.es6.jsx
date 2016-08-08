b54c1a0c6ebfa99f7a2b4607d40e618dbf073178
Bill Bushey <wbushey@gmail.com>
2016-08-07 20:42:39 -0500

    Removing unneeded server side partials Rendering Edit Profile via React

--- app/assets/javascripts/components/views/edit_profile.es6.jsx
+++ app/assets/javascripts/components/views/edit_profile.es6.jsx
@@ -7,6 +7,13 @@ class EditProfile extends React.Component{
         <TextField name='username' label="Username" private={false}/>
         <ShippingInformation/>
         <Survey/>
+        <h2>Change Password</h2>
+        <PasswordField name='password' label='New Password' private={true}/>
+        <PasswordField name='current_password' label='Current Password' private={true}/>
+        <fieldset className='form-actions'>
+          <input className='btn btn-primary' name='commit' type='submit' value='Update'/>
+          <a className='btn'>Back</a> 
+        </fieldset>
       </form>
     );
   }
