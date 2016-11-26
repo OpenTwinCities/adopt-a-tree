f27c06c9ed65c238f5eff57f9b247082c86be837
Bill <wbushey@gmail.com>
2016-11-26 17:37:09 -0600

    Updating Capistrano deploy to use OpenTwinCities repo Fixes #50

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'Adopt-A-Tree'
-set :repo_url, 'git@github.com:ballPointPenguin/adopt-a-tree.git'
+set :repo_url, 'git@github.com:OpenTwinCities/adopt-a-tree.git'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
