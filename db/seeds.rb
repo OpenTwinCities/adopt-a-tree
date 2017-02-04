d29660bd73464743c06f940f506c07f14597332d
Jason Hsu <jhsu802701@jasonhsu.com>
2017-02-04 22:33:19 +0000

    Feed only 1 out of 10 trees in *.json file into database (saves time during seeding process)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+n = 10
+
 trees_file_path = Rails.root.join('db', 'trees.json')
 puts "Reading and parsing #{trees_file_path}"
+puts "NOTE: To save time during the seeding process,"
+puts "only 1 out of #{n} trees will be fed into the database."
 trees_file      = File.read(trees_file_path)
 trees_hash      = JSON.parse(trees_file)
 
@@ -19,19 +23,21 @@ puts "Number of trees: #{num_trees}"
 i = 0
 
 trees_data.each do |tree|
-  Thing.find_or_create_by!(mpls_id: tree[8]) do |t|
-    t.properties = {}
-    tree_fields.map.with_index do |f, index|
-      t.properties[tree_fields[index]] = tree[index]
-    end
+  if i % n == 0
+    Thing.find_or_create_by!(mpls_id: tree[8]) do |t|
+      t.properties = {}
+      tree_fields.map.with_index do |f, index|
+        t.properties[tree_fields[index]] = tree[index]
+      end
 
-    t.mpls_unique = tree[9]
-    t.lng         = tree[64]
-    t.lat         = tree[65]
-    t.species     = tree[66]
+      t.mpls_unique = tree[9]
+      t.lng         = tree[64]
+      t.lat         = tree[65]
+      t.species     = tree[66]
+    end
   end
   i += 1
-  print " #{i}/#{num_trees} " if (i % 100).zero?
+  print " #{i}/#{num_trees} processed" if (i % 100).zero?
 end
 
 puts "Finished processing #{num_trees} trees"
