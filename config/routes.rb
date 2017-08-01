8e4a21c6cf8bc4734bd8db8ea0a46e82561fa750
Jason Hsu <jhsu802701@jasonhsu.com>
2017-08-01 23:01:26 +0000

    Comments and notes updated only (by outlining script)

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@
 #          promo_codes_use GET    /promo_codes/use(.:format)       promo_codes#use
 #              rails_admin        /admin                           RailsAdmin::Engine
 #                     root GET    /                                main#index
-#
+# 
 # Routes for RailsAdmin::Engine:
 #   dashboard GET         /                                      rails_admin/main#dashboard
 #       index GET|POST    /:model_name(.:format)                 rails_admin/main#index
@@ -56,7 +56,7 @@
 #        edit GET|PUT     /:model_name/:id/edit(.:format)        rails_admin/main#edit
 #      delete GET|DELETE  /:model_name/:id/delete(.:format)      rails_admin/main#delete
 # show_in_app GET         /:model_name/:id/show_in_app(.:format) rails_admin/main#show_in_app
-#
+# 
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
