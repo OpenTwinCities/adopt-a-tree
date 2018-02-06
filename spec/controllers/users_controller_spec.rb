d6139700cd990f832705926ecd2c933bfd917a88
Jason Hsu <jhsu802701@jasonhsu.com>
2018-02-06 03:06:07 +0000

    Updated positional arguments in should create user test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -30,9 +30,9 @@ RSpec.describe UsersController, :type => :controller do
   context '#create' do
     context 'with valid parameters' do
       it 'should create user' do
-        post :create, format: 'json', user: {email: 'user1@example.com', first_name: 'Userf1', last_name: 'Userl', 
+        post :create, params: {format: 'json', user: {email: 'user1@example.com', first_name: 'Userf1', last_name: 'Userl', 
           password: 'correcT*1', password_confirmation: 'correcT*1', username: 'userfuserl1',
-          address_1: '350 S 5th St', city: 'Minneapolis', state: 'MN', zip: 55415}
+          address_1: '350 S 5th St', city: 'Minneapolis', state: 'MN', zip: 55415}}
         assert_response :success
       end
 
